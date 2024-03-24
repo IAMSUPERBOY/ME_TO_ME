60c54e436a91923315164b571956b829a0b01d3d
Suraj Krishna S.S <117973059+IAMSUPERBOY@users.noreply.github.com>
2024-03-24 14:04:31 +0530

    UI and Load message button has been added

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"], // Adjust paths to your HTML and JS files
+    theme: {
+      extend: {}, // Add your theme customizations here
+    },
+    plugins: [], // Add any Tailwind CSS plugins you want to use
+  }
+  
